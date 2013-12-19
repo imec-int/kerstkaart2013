de9c74b49ab610c445bb80edf569d6393c479350
Sam Decrock <sam.decrock@gmail.com>
2013-12-19 16:28:57 +0100

    updated the rescale_images script

--- app/setup/rescale_images.js
+++ app/setup/rescale_images.js
@@ -1,6 +1,6 @@
-// *****************************************************************
-// *** this script just rescales some images (not really needed) ***
-// *****************************************************************
+// ****************************************************************************
+// *** this script just rescales some images (can come in handy some times) ***
+// ****************************************************************************
 
 var fs = require('fs');
 var path = require('path');
@@ -9,23 +9,39 @@ var printf = require('printf');
 var config = require('../config');
 var image = require('../image');
 
-var sourcefolder = '../../data/some_images/'
-var outputfolder = '../../data/raw_bootstrap_images/'
+var USE_NEW_NAME = false;
+var NEW_NAME_REGEX  = '_twitterimage_%05d.jpg';
+var NEW_NAME_COUNTER = 0; // start number of each converted image
 
-var counter = 0; // start number of each converted image
+var WIDTH = 256;
+var HEIGHT = 256;
+
+// var sourcefolder = '/Users/sam/Documents/lode_images/';
+var sourcefolder = '/Users/sam/Dropbox/MiX/kerstkaart2013/';
+var outputfolder = '../../data/raw_bootstrap_images/';
+
+
+
 fs.readdir(sourcefolder, function (err, files) {
-	console.log(err);
+	if(err) return console.log(err);
 
 	async.eachLimit(files, 10, function (file, $){
+
 		var absoluteFile = path.join( sourcefolder, file );
 		if(!file.match(/.jpg$|.png$/)) return $(); // only allow those file extensions
 
-		var outputfile = printf('image_%05d.jpg', counter++);
-		var absoluteOutputfile = path.join(outputfolder, outputfile);
+		var absoluteOutputfile;
+
+		if(USE_NEW_NAME){
+			var outputfile = printf(NEW_NAME_REGEX, NEW_NAME_COUNTER++);
+			absoluteOutputfile = path.join(outputfolder, outputfile);
+		}else{
+			absoluteOutputfile =  path.join(outputfolder, file);
+		}
 
 		console.log(absoluteOutputfile);
 
-		image.crop(absoluteFile, 200, 200, absoluteOutputfile, $);
+		image.crop(absoluteFile, WIDTH, HEIGHT, absoluteOutputfile, $);
 	}, function (err) {
 		if(err) return console.log(err);
 		return console.log("> DONE");
