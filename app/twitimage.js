68adbbc3e9fcf009b3f6fc356d6a1c294b50a6eb
Matthias De Geyter <matthias.degeyter@ibbt.be>
2013-12-20 21:15:34 +0100

    posting to twitpic works; the one who tweets the link can't expand the image, but others can. btw: twitpic has the intelligence to detect when an image was already (at least by the same user) posted!

---
+++ app/twitimage.js
@@ -0,0 +1,29 @@
+var keys = require('./config').keys;
+var fs = require('fs');
+var OAuthEcho = require('oauth').OAuthEcho;
+
+var realm = "http://api.twitter.com/";
+var verifyCredentials = "https://api.twitter.com/1.1/account/verify_credentials.json";
+var oauth = new OAuthEcho(realm, verifyCredentials, keys.consumerKey, keys.consumerSecret, "1.0", "HMAC-SHA1", null,{ "X-Auth-Service-Provider": verifyCredentials});
+
+function twitPicImage(imagePath, callback) {
+	var media = "data:image/jpeg;base64," + fs.readFileSync(imagePath, 'base64');
+	oauth.post("http://api.twitpic.com/2/upload.json", keys.token, keys.tokenSecret, {media: media, key: keys.twitPic}, function(err, data){
+		if(err) return callback(err);
+		var url = "";
+		try{
+			url = JSON.parse(data).url;
+		}catch(err){
+			return callback(err);
+		}
+		callback(err, url);
+
+	});
+}
+
+// example usage
+// twitPicImage(require('path').join(__dirname, 'public/images/santamosaic.jpg'), function(err, url){
+// 	console.log(url);
+// });
+
+exports.twitPicImage = twitPicImage;
