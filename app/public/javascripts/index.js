7d85275a1952e61b534aa48dfb429c12cf796425
Robby Wauters <robbywauters@gmail.com>
2013-12-23 17:10:07 +0100

    Identify mail address from URL param "mail" [Mixpanel]

--- app/public/javascripts/index.js
+++ app/public/javascripts/index.js
@@ -5,6 +5,21 @@ $(function(){
 	var upload = new Upload({flyingTiles: flyingTiles});
 	upload.init();
 
-	// TODO: mixpanel.identify("some email address");
+	var mailid = urlParam('mail')
+	if(mailid){
+		var mail = decodeURI(mailid);
+		console.log("mail: "+mail);
+		mixpanel.identify(mail);
+	}
 	mixpanel.track("Page View");
+
+	function urlParam(name){
+	    var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
+	    if (results==null){
+	       return null;
+	    }
+	    else{
+	       return results[1] || 0;
+	    }
+	}
 });
