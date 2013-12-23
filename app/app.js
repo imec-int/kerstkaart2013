b754d28a02524742cf5707741a5e46f8a9523fe3
Robby Wauters <robbywauters@gmail.com>
2013-12-23 16:51:17 +0100

    share tekstje

--- app/app.js
+++ app/app.js
@@ -324,13 +324,4 @@ function getSomeRandomTilesWithPosition(nrOfTiles, callback){
 	});
 }
 
-// YOUNES stuff:
 
-app.get('/share', function(req, res){
-	res.render('share', {
-		title     	: 'Share Your MiX Kerstkaart 2013',
-		message 	: 'iwasmixed - We wish you amazing holydays.',
-		imgUrl		: 'http://d3j5vwomefv46c.cloudfront.net/photos/large/828376205.jpg',
-		pageUrl		: 'http://twitpic.com/dp6z65'
-	});
-});
--- app/public/javascripts/sharing.js
+++ app/public/javascripts/sharing.js
@@ -1,6 +1,6 @@
 var Sharing = {
 	renderButtons: function (data) {
-		data.message = encodeURIComponent(data.message);
+		// data.message = encodeURIComponent(data.message);
 
 		$('.share').empty();
 		$('.share').append(  $("#share-template").tmpl(data) );
