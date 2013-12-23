467e137fb29c98efaf2ccae619f2ca5b2e307d44
Robby Wauters <robbywauters@gmail.com>
2013-12-23 15:20:17 +0100

    main pageview analytics + TODO email-adres

--- app/public/javascripts/index.js
+++ app/public/javascripts/index.js
@@ -5,5 +5,6 @@ $(function(){
 	var upload = new Upload({flyingTiles: flyingTiles});
 	upload.init();
 
-	mixpanel.track("Load complete");
+	// TODO: mixpanel.identify("some email address");
+	mixpanel.track("Page View");
 });
