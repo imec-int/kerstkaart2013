cb9f9bf00eac1cf516273497d08fe9f8d3a7bad3
Sam Decrock <sam.decrock@gmail.com>
2013-12-23 18:18:19 +0100

    updated sharing layout

--- app/public/javascripts/sharing.js
+++ app/public/javascripts/sharing.js
@@ -2,7 +2,7 @@ var Sharing = {
 	renderButtons: function (data) {
 		// data.message = encodeURIComponent(data.message);
 
-		$('.share').empty();
-		$('.share').append(  $("#share-template").tmpl(data) );
+		$('.sharewrapper').empty();
+		$('.sharewrapper').append(  $("#share-template").tmpl(data) );
 	}
 }
