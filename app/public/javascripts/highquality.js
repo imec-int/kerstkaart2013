03d270b8effba8eac08eeed4a9acaed6f5aa4c9c
Robby Wauters <robbywauters@gmail.com>
2013-12-23 22:37:34 +0100

    High Quality Success track

--- app/public/javascripts/highquality.js
+++ app/public/javascripts/highquality.js
@@ -4,7 +4,7 @@ var HighQuality = function (options){
 	var init = function (){
 		$.post('/api/renderhq', {userid: window.userid}, function (data) {
 			if(data.err) return console.log(data.err);
-
+			mixpanel.track("High Quality Success",{"userid":window.userid});
 			window.location = data.mosaicimageHQ;
 		});
 	};
