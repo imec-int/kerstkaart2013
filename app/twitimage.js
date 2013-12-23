bb9f8b2b6a339de101f7f5c1f1902c80b26f45ec
Matthias De Geyter <matthias.degeyter@ibbt.be>
2013-12-23 18:12:22 +0100

    actually, image files are pngs, not that it matters for twitpic

--- app/twitimage.js
+++ app/twitimage.js
@@ -7,7 +7,7 @@ var verifyCredentials = "https://api.twitter.com/1.1/account/verify_credentials.
 var oauth = new OAuthEcho(realm, verifyCredentials, keys.consumerKey, keys.consumerSecret, "1.0", "HMAC-SHA1", null,{ "X-Auth-Service-Provider": verifyCredentials});
 
 function twitPicImage(imagePath, callback) {
-	var media = "data:image/jpeg;base64," + fs.readFileSync(imagePath, 'base64');
+	var media = "data:image/png;base64," + fs.readFileSync(imagePath, 'base64');
 	oauth.post("http://api.twitpic.com/2/upload.json", keys.token, keys.tokenSecret, {media: media, key: keys.twitPic}, function(err, data){
 		if(err) return callback(err);
 		var url = "";
